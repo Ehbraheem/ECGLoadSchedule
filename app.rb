72db52a65a810c58b6af259e2300b387efa3567d
derrick <40855937+Derrickvibe@users.noreply.github.com>
2023-03-31 22:30:22 +0000

    implemented UI designs

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ require_relative "./config/environments" # database configuration
 
 # Application Entry Point
 class App < Sinatra::Base
+  configure do
+    set :public_folder, "public"
+    set :views, "app/views"
+  end
   get "/search" do
+    erb :index
   end
 end
